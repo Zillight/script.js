de64cedc60e65ce13fba5f52ef495321dddda545
Banji Zillight <banjiolawolez@gmail.com>
2023-02-13 14:51:40 +0100

    ways to write variable and strings

--- first.js
+++ first.js
@@ -1,3 +1,5 @@
 // Print strings and variables in a single line
 let city = "Abuja.";
+let time = '18:00';
 console.log("City:",city);
+console.log("City: " + city); // Notice the difference! space in front of city and + in place of ,.

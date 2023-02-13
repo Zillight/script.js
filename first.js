3cf14d714753c1cc54019ee306864420982f11a3
Banji Zillight <banjiolawolez@gmail.com>
2023-02-13 14:27:11 +0100

    Typeof Operation

--- first.js
+++ first.js
@@ -1,10 +1,16 @@
-// The Ritual
-console.log("Hello, World."); 
+// Typeof()
 
-console.log("Hello, Team.") 
-// With or without semi-colon, either way works but I chose to do with semi-colon.
+let w = "Hello World";
+let x = 80;
+let y = 8.5;
+let z = true;
 
+console.log(w);
+console.log(x);
+console.log(y);
+console.log(z);
 
-console.log(80); // Integer
-
-console.log(8.5); // Float
+console.log(typeof(w)); // string
+console.log(typeof(x)); // number
+console.log(typeof(y)); // number
+console.log(typeof(z)); // boolean

9b1ea7d9639c4a47b4bf8c66ad2f114ecf0c6a9b
Banji Zillight <banjiolawolez@gmail.com>
2023-02-13 14:56:49 +0100

    putting spaces

--- first.js
+++ first.js
@@ -1,5 +1,10 @@
 // Print strings and variables in a single line
 let city = "Abuja.";
-let time = '18:00';
+let time = '18:00.';
 console.log("City:",city);
 console.log("City: " + city); // Notice the difference! space in front of city and + in place of ,.
+console.log("City:",city, "Time:", time);
+console.log("City:" + city + "Time:" + time); //No space
+console.log("City:" + " " + city + " " + "Time:" + " " + time); //No space
+
+//Template literal

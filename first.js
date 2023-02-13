555d0e9b15d329b6d634f205bf8b446d7ea7f5ab
Banji Zillight <banjiolawolez@gmail.com>
2023-02-13 14:38:29 +0100

    Reassign

--- first.js
+++ first.js
@@ -1,11 +1,7 @@
 // Variable Reassigning
 
-let firstName = "Banji"
-let surname = "Olawole"
-let thirdName = "Olakunle"
-
-console.log(firstName + " " + thirdName + " " + surname + ".");
-// Prints Banji Olakunle Olawole
-
-thirdName = "Zillight";
-console.log(firstName + " " + thirdName + " " + surname + ".");
+let name = "Banji"
+let nickname = "Zillight"
+console.log(name); //Banji
+name = nickname;
+console.log(name); //Zillight

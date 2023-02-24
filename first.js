bf5285198d49849d20aab766ea8bc1ad2b3bade4
Banji Zillight <banjiolawolez@gmail.com>
2023-02-24 11:03:25 +0100

    Commenting i JS

--- first.js
+++ first.js
@@ -1,11 +1,7 @@
-// Print strings and variables in a single line
-let city = "Abuja.";
-let time = '18:00.';
-console.log("City:",city);
-console.log("City: " + city); // Notice the difference! space in front of city and + in place of ,.
-console.log("City:",city, "Time:", time);
-console.log("City:" + city + "Time:" + time); //No space
-console.log("City:" + " " + city + " " + "Time:" + " " + time); //No space
+// "//" is used for commneting in javascript
 
-//Template literal
-console.log(`City: ${city}`);
+// Cmd + / can also be used to comment single and multiple lines
+// Commneting in javascript
+// Commneting in javascript
+// Commneting in javascript
+// Commneting in javascript

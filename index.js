dbc76cc310ce49b10bf5af46fb462eeccc4588b8
Banji Zillight Olawole <107891531+Zillight@users.noreply.github.com>
2023-02-24 10:47:41 +0000

    Using commit

---
+++ index.js
@@ -0,0 +1,2 @@
+const my_name = prompt("Enter your name: ");
+console.log("Hello," + my_name);
--- second.js
+++ second.js
@@ -1,2 +1,4 @@
 const my_name = prompt("Enter your name: ");
 console.log("Hello," + my_name);
+
+//Using Promt

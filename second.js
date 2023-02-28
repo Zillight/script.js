d7f8f9ade0e1f749ed81152fd3a9a8c478b1c2ca
Banji Zillight <banjiolawolez@gmail.com>
2023-02-28 19:19:53 +0100

    Prompt saves as strings

--- second.js
+++ second.js
@@ -1,3 +1,7 @@
+//Javascript Number Input + Typeof Operator
 const prompt = require("prompt-sync")();
-const my_name = prompt("Enter your name: ");
-console.log("Hello," + my_name);
+
+const age = prompt("Enter you age: ");
+console.log('You are', age); //but prompt saves input as strings
+
+console.log(typeof(age));

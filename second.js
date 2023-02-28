0c81f9a09c7f23f5790b69403a018e183c762386
Banji Zillight <banjiolawolez@gmail.com>
2023-02-28 19:14:12 +0100

    Cont...

--- second.js
+++ second.js
@@ -1,2 +1,4 @@
+const prompt = require("prompt-sync")();
 const my_name = prompt("Enter your name: ");
-console.log("Hello," + my_name);
+console.log("Hello,", my_name);
+console.log("Hello,"+ my_name); //Adds the strings and the variable together without adding space

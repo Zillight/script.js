4382b02e2988973f8215a0101c115422ee10a554
Banji Zillight <banjiolawolez@gmail.com>
2023-02-28 19:21:59 +0100

    Using parseInt to convert strings to Numbers

--- second.js
+++ second.js
@@ -1,7 +1,8 @@
 //Javascript Number Input + Typeof Operator
+//To ensure we have integer input, we use parseInt()
 const prompt = require("prompt-sync")();
 
-const age = prompt("Enter you age: ");
+const age = parseInt(prompt("Enter you age: "));
 console.log('You are', age); //but prompt saves input as strings
 
 console.log(typeof(age));

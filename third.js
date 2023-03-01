454771272aeed1b0c0586133729290aa005bcded
Banji Zillight <banjiolawolez@gmail.com>
2023-03-01 14:19:26 +0100

    Type casting

--- third.js
+++ third.js
@@ -1,21 +1,7 @@
-// Type Conversion
+// Explicit Numeric Conversion
 
-let result = "4" - "Hello";
-let result1 = "4" - true;
-let result2 = '4' + true;
-let result3 = '4' + false;
-let result4 = 4 + false;
+let result = Number("234");
 
-console.log(result); // Prints NaN - Not a Number
-console.log(result1); // Prints 3. true = 1. false = 0.
-console.log(result2); // Prints 4true
-console.log(result3); // Prints 4false
-console.log(result4); // Prints 4
+console.log(result);
 
-
-console.log(typeof(result)); // Prints NaN - Not a Number
-console.log(typeof(result1)); // Prints 3. true = 1. false = 0.
-console.log(typeof(result2)); // Prints string
-console.log(typeof(result3)); // Prints string
-console.log(typeof(result4)); // Prints number
-
+console.log(typeof(result));

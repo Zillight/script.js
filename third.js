8025d0c38ba283e95f056293f95a0ffe723c460e
Banji Zillight <banjiolawolez@gmail.com>
2023-03-01 13:52:51 +0100

    Both prints number

--- third.js
+++ third.js
@@ -5,6 +5,6 @@ const result2 = "5" * 2;
 
 
 console.log(result1); // Prints 10
-console.log(result2); //Prints 3
-console.log(typeof(result1)); // String - String(Number) = Number. Provided the value is a number 
-//console.log(typeof(result2)); // Both prints number
+console.log(result2); //Prints 10
+console.log(typeof(result1)); // String * String(Number) = Number. Provided the value is a number 
+console.log(typeof(result2)); // Both prints number

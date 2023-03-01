91c9c82bba511fb5b2467a8fd8a0420253f130a6
Banji Zillight <banjiolawolez@gmail.com>
2023-03-01 14:13:32 +0100

    String(no value) + Boolean = String

--- third.js
+++ third.js
@@ -2,8 +2,13 @@
 
 let result = "4" - "Hello";
 let result1 = "4" - true;
+let result2 = '4' + true;
+
 console.log(result); // Prints NaN - Not a Number
 console.log(result1); // Prints 3. true = 1. false = 0.
+console.log(result2); // Prints 4true
+
 console.log(typeof(result)); // Prints NaN - Not a Number
 console.log(typeof(result1)); // Prints 3. true = 1. false = 0.
+console.log(typeof(result2)); // Prints string
 

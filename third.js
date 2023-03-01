2c83c161d43f3845c704cbe61a7dca95727b358b
Banji Zillight <banjiolawolez@gmail.com>
2023-03-01 14:11:11 +0100

    Number(string) - Boolean = Number

--- third.js
+++ third.js
@@ -1,5 +1,9 @@
 // Type Conversion
 
 let result = "4" - "Hello";
+let result1 = "4" - true;
 console.log(result); // Prints NaN - Not a Number
+console.log(result1); // Prints 3. true = 1. false = 0.
+console.log(typeof(result)); // Prints NaN - Not a Number
+console.log(typeof(result1)); // Prints 3. true = 1. false = 0.
 

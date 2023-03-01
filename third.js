829b8d74d85abc78edda7e3869c152ffef856711
Banji Zillight <banjiolawolez@gmail.com>
2023-03-01 13:08:21 +0100

    Not equal to

--- third.js
+++ third.js
@@ -5,3 +5,4 @@
 console.log(8>5); // prints true
 console.log('5'==5); // prints true because == checks the value and not the datatype.
 console.log('5'===5); // prints false bacause === strictly checks the datatype and the value
+console.log('5'!==5); // They are not exactly the same.

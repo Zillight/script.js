f636c6159e33ade12fc4721d80fda88074a16adc
Banji Zillight <banjiolawolez@gmail.com>
2023-03-01 13:48:39 +0100

    String-String(Number) = Number. with number value

--- third.js
+++ third.js
@@ -1,6 +1,10 @@
 // Type Conversion
 
 const result1 = "5" - "2";
+const result2 = "5" - 2;
 
 
-console.log(result1);
+console.log(result1); // Prints 3
+console.log(result2); //Prints 3
+console.log(typeof(result1)); // String - String(Number) = Number. Provided the value is a number 
+console.log(typeof(result2)); // Both prints number

3079529db5b480853098de2ee9cf1601d01783a1
Banji Zillight <banjiolawolez@gmail.com>
2023-03-01 13:41:26 +0100

    + concatenates strings and numbers

--- third.js
+++ third.js
@@ -1,14 +1,6 @@
-// Logical Operator
+// Type Conversion
 
-// && AND Operator - Both
-// || OR Operator - Either
-// ! Not Operator - -Ve 
-
-const age = 18;
-const height = 5.5;
-let result = age >= 18 && height < 5;
-console.log(result); // Prints false
-result = height > 6;
-console.log(result); // Prints false
-console.log(!result); // Prints true, ought to be false but ! operator changed it.
-console.log(age < 18 || height >= 5); //Prints true because either is true
+const x = 8;
+const y = "4";
+const result = x + y ;
+console.log(result); // Prints 84

b97e91b9be1c23aa3b4ff8c1d8e3e9850c84d822
Banji Zillight <banjiolawolez@gmail.com>
2023-03-01 13:36:48 +0100

    Operator Done

--- third.js
+++ third.js
@@ -10,4 +10,5 @@ let result = age >= 18 && height < 5;
 console.log(result); // Prints false
 result = height > 6;
 console.log(result); // Prints false
-console.log(!result); // Prints true
+console.log(!result); // Prints true, ought to be false but ! operator changed it.
+console.log(age < 18 || height >= 5); //Prints true because either is true

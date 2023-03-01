8164131bad38f5b1181a1850e8a07a5ce1c6bc19
Banji Zillight <banjiolawolez@gmail.com>
2023-03-01 14:29:27 +0100

    Ternary Operator

--- third.js
+++ third.js
@@ -1,1 +1,9 @@
-// 
+// Ternary Operator
+
+const age = 15;
+
+(age>=18) ? console.log('can vote'): console.log('cannot vote');
+
+// Used for a single line of conditional statement.
+
+//Syntax: (condition) ? execute if true : execute if false(else);

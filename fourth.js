9373978963150c13313290d2e4b35e4024cee4e0
Banji Zillight <banjiolawolez@gmail.com>
2023-03-02 21:01:09 +0100

    While loop syntax

--- fourth.js
+++ fourth.js
@@ -1,31 +1,7 @@
 // Decision Making
-// Switch Statement
+// While loop
 
-// Simple Calculator
-
-const prompt = require("prompt-sync")();
-
-const operator = prompt("Enter Operator (either + , - , *  or /):"); // take the Operator input
-
-// take the operand input
-
-const number1 = parseFloat(prompt(`Enter first number: `));
-const number2 = parseFloat(prompt(`Enter second number: `));
-let result;
-switch (operator){
-    case `+`:
-        result = number1 + number2;
-    break;
-    case `-`:
-        result = number1 - number2;
-    break;
-    case `*`:
-        result = number1 * number2;
-    break;
-    case `/`:
-        result = number1 / number2;
-    break;
-    default:
-        console.log(`Invalid Operator`);
-}
-console.log(`${number1} ${operator} ${number2} = ${result}.`);
+// Syntax: 
+// while (condition){
+//     statement in the while
+// }

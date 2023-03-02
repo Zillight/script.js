3f8be511f52c85956dd33b472731879312c269ba
Banji Zillight <banjiolawolez@gmail.com>
2023-03-02 21:12:55 +0100

    Multiplication table with while loop

--- fourth.js
+++ fourth.js
@@ -1,10 +1,14 @@
 // Decision Making
 // While loop
 
+const prompt = require('prompt-sync')();
+
+const number = parseFloat(prompt(`Enter a number: `));
+
 let count = 1;
-while (count < 5){
-    console.log(count);
-    console.log(`Learning while loop`);
-    console.log(`Javascript is fun`);
+
+while (count <= 10){
+    let product = number * count;
+    console.log(`${number} * ${count} = ${product}.`);
     count ++;
 }

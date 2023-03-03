3ea8bceeb33430e1722fe6cd562639c693763c74
Banji Zillight <banjiolawolez@gmail.com>
2023-03-03 10:10:06 +0100

    Reason for break

--- fourth.js
+++ fourth.js
@@ -6,3 +6,5 @@ for (let i = 1; i <= 5; i++){
     break;
     console.log(`After break`) // This has grey connotation because nothing gets executed after break
 }
+
+// Prints 1 only because of the break statement

0f521e483d85699044f0c005dbc23f7faf1651e6
Banji Zillight <banjiolawolez@gmail.com>
2023-03-04 19:38:48 +0100

    making local variable global

--- fifth.js
+++ fifth.js
@@ -1,9 +1,10 @@
 // Variable Scope (Local & Global)
+// To make local variable global
 
+let result;
 function addNumbers(n1, n2){
-    let result = n1 = n2; // Local variable
+    result = n1 = n2; // Local variable... ensure that it is not declared here
     console.log(result);
 }
-//let result = 123; // Global variable
 addNumbers(10, 7);
 console.log(result);

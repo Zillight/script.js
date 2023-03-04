3aa04cea38015ca7c09285acb79198eef7883f75
Banji Zillight <banjiolawolez@gmail.com>
2023-03-04 19:35:40 +0100

    prints error without the global scope

--- fifth.js
+++ fifth.js
@@ -4,6 +4,6 @@ function addNumbers(n1, n2){
     let result = n1 = n2; // Local variable
     console.log(result);
 }
-let result = 123; // Global variable
+//let result = 123; // Global variable
 addNumbers(10, 7);
 console.log(result);

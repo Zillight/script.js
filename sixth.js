745d0ab7744e7b579d76ad06b8d58a73c7fc73f8
Banji Zillight <banjiolawolez@gmail.com>
2023-03-08 14:20:39 +0100

    Using charAt

--- sixth.js
+++ sixth.js
@@ -6,11 +6,12 @@ const text = `I am ${age}years old`;
 console.log(text); // I am 20years old
 console.log(typeof(text)); // string
 
-// Using 'bracket notation' to call out the characters in the string 
-console.log(text[0]); // I
-console.log(text[1]); // space
-console.log(text[2]); // a
-console.log(text[3]); // m
-console.log(text[4]); // space
-console.log(text[5]); // 2
-console.log(text[6]); // 0
+// Using charAt
+
+console.log(text.charAt(0)); // I
+console.log(text.charAt(1)); // space
+console.log(text.charAt(2)); // a
+console.log(text.charAt(3)); // m
+console.log(text.charAt(4)); // space
+console.log(text.charAt(5)); // 2
+console.log(text.charAt(6)); // 0

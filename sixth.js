b073b40fd49f657d36e8b73ccfc0acadd80df964
Banji Zillight <banjiolawolez@gmail.com>
2023-03-08 14:33:12 +0100

    toLowerCase.

--- sixth.js
+++ sixth.js
@@ -1,9 +1,10 @@
 // Strings Methods
 // 1. toUpperCase() and toLowerCase() Method
 
-const text = ` I love Jesus`
+const text = `I love Jesus`
 const result = text.toUpperCase(); // Note: Do not forget to type the U and C in caps
-console.log(result);
+console.log(result); // I LOVE JESUS
 
-const maxim = `MONEY is EVERYTHING`;
-console.log(maxim.toLowerCase()); // Dont forget to type the L and C a-in caps and add ().
+const maxim = `MONEY is not EVERYTHING`;
+console.log(maxim.toLowerCase()); // money is not everything
+// Dont forget to type the L and C a-in caps and add ().

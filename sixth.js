681175b7973830b5d50e3923b663b40fc8d725f4
Banji Zillight <banjiolawolez@gmail.com>
2023-03-08 14:25:12 +0100

    String length

--- sixth.js
+++ sixth.js
@@ -6,12 +6,9 @@ const text = `I am ${age}years old`;
 console.log(text); // I am 20years old
 console.log(typeof(text)); // string
 
-// Using charAt
+// String length
+ const result = text.length;
+ console.log(result); // prints 16
 
-console.log(text.charAt(0)); // I
-console.log(text.charAt(1)); // space
-console.log(text.charAt(2)); // a
-console.log(text.charAt(3)); // m
-console.log(text.charAt(4)); // space
-console.log(text.charAt(5)); // 2
-console.log(text.charAt(6)); // 0
+ let greet = `hello`;
+ console.log(greet, greet.length); // Prints hello 5

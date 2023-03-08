93e43bcb0882ca8841af3119b531c515c3b10245
Banji Zillight <banjiolawolez@gmail.com>
2023-03-08 14:28:05 +0100

    String Method - toUpperCase()

--- sixth.js
+++ sixth.js
@@ -1,14 +1,6 @@
-// Strings
+// Strings Methods
+// 1. toUpperCase() and toLowerCase() Method
 
-const age = 20;
-const text = `I am ${age}years old`;
-
-console.log(text); // I am 20years old
-console.log(typeof(text)); // string
-
-// String length
- const result = text.length;
- console.log(result); // prints 16
-
- let greet = `hello`;
- console.log(greet, greet.length); // Prints hello 5
+const text = ` I love Jesus`
+const result =text.toUpperCase();
+console.log(result);

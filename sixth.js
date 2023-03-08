8a9b4c5c85d326cf17c208392a8fcd5094d25a77
Banji Zillight <banjiolawolez@gmail.com>
2023-03-08 14:51:33 +0100

    slice() method

--- sixth.js
+++ sixth.js
@@ -1,15 +1,7 @@
 // Strings Methods
-// Split() Method
-
-const text = `Hello my name is Zillight`;
-
-const result = text.split(` `);
-console.log(result); // [ 'Hello', 'my', 'name', 'is', 'Zillight' ]
-
-// splits into an array of strings, each word becoming a string in the sentence. 
+// Slice() Method
 
 const letter = `Zillight`;
-const result1 = letter.split(``);
-console.log(result1); // [ 'Z', 'i', 'l', 'l', 'i', 'g', 'h', 't' ]
-
-// Note: Do not forget to put "" and don't put space inbetween them.
+const result = letter.slice(3, 8);
+// Slice() takes in 2 parameters (start(at 0), end+1)
+console.log(result); // light

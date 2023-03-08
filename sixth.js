491af6c95bfb2e9ab7659f8cc7ff4eea72b2a101
Banji Zillight <banjiolawolez@gmail.com>
2023-03-08 14:37:28 +0100

    concat() method

--- sixth.js
+++ sixth.js
@@ -1,10 +1,8 @@
 // Strings Methods
-// 1. toUpperCase() and toLowerCase() Method
+// 2. concat() Method
 
-const text = `I love Jesus`
-const result = text.toUpperCase(); // Note: Do not forget to type the U and C in caps
-console.log(result); // I LOVE JESUS
+const text1 = `Javascript`;
+const text2 = `programming`;
 
-const maxim = `MONEY is not EVERYTHING`;
-console.log(maxim.toLowerCase()); // money is not everything
-// Dont forget to type the L and C a-in caps and add ().
+const result = text1.concat(text2);
+console.log(result); // Javascriptprogramming

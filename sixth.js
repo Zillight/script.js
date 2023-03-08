0ff712d0f4aa8c1c53a55f2aba2fb244832ade7a
Banji Zillight <banjiolawolez@gmail.com>
2023-03-08 15:01:50 +0100

    search().

--- sixth.js
+++ sixth.js
@@ -3,5 +3,8 @@
 
 const letter = `Javascript is fun`;
 const result = letter.search(`fun`);
-// seraches for the substring in the given string
+// searches for the substring in the given string
 console.log(result); // 14
+
+const result1 = letter.search(`funny`);
+console.log(result1); // -1 indicates that no such word can be found in the string

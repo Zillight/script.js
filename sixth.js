97cd19cf295c732c9d82818416ad97107e43bc23
Banji Zillight <banjiolawolez@gmail.com>
2023-03-08 14:58:09 +0100

    Trim() method

--- sixth.js
+++ sixth.js
@@ -1,7 +1,7 @@
 // Strings Methods
-// Slice() Method
+// Trim() Method
 
-const letter = `Zillight`;
-const result = letter.slice(3, 8);
-// Slice() takes in 2 parameters (start(at 0), end+1)
-console.log(result); // light
+const letter = `        Zillight`;
+const result = letter.trim();
+// cleans up the white spaces in the string
+console.log(result); 

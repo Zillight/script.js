98c66c61ae8171c77df234c9e0295cf09c73527c
Banji Zillight <banjiolawolez@gmail.com>
2023-03-08 14:42:38 +0100

    split() method

--- sixth.js
+++ sixth.js
@@ -1,7 +1,9 @@
 // Strings Methods
-// 2. Replace() Method
+// 2. Split() Method
 
-const text = `Hello, my name is Zillight`;
+const text = `Hello my name is Zillight`;
 
-const result = text.replace(`Hello`, `Bonjour`);
-console.log(result); // Bonjour, my name is Zillight
+const result = text.split(` `);
+console.log(result); // [ 'Hello', 'my', 'name', 'is', 'Zillight' ]
+
+// splits into an array of strings, each word becoming a string in the sentence. 

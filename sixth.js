6a2d599e2c26498be0310e8d74fe3901631b0dc0
Banji Zillight <banjiolawolez@gmail.com>
2023-03-08 13:54:47 +0100

    Altering object property using dot notation

--- sixth.js
+++ sixth.js
@@ -6,5 +6,9 @@ const person = {
     age: 2
 }
 
+console.log(person); // Prints 2
 console.log(person.name); // Prints Ara
+console.log(person.age); // Prints 2
+person.age = 7;
+console.log(person); // Prints 2
 console.log(person.age); // Prints 2

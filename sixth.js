c06d6e3cb5674534b31eebf1335a424fb36f033a
Banji Zillight <banjiolawolez@gmail.com>
2023-03-08 13:49:44 +0100

    Object in JS

---
+++ sixth.js
@@ -0,0 +1,9 @@
+// Objects in JS
+// Note: Object is a datatype in JS just like strings and numbers
+
+const person = {
+    name:'Ara',     // Object properties
+    age: 2
+}
+
+console.log(person); // Prints { name: 'Ara', age: 2 }

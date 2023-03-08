0325fdc2d87cb72ca5c5612a886ba767c5890067
Banji Zillight <banjiolawolez@gmail.com>
2023-03-08 14:18:23 +0100

    Accesing the characters in the string

--- sixth.js
+++ sixth.js
@@ -5,3 +5,12 @@ const text = `I am ${age}years old`;
 
 console.log(text); // I am 20years old
 console.log(typeof(text)); // string
+
+// Using 'bracket notation' to call out the characters in the string 
+console.log(text[0]); // I
+console.log(text[1]); // space
+console.log(text[2]); // a
+console.log(text[3]); // m
+console.log(text[4]); // space
+console.log(text[5]); // 2
+console.log(text[6]); // 0

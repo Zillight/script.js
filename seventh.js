98bfa2f03c9cf9e71f8ccb6d7e79234aaa4fa673
Banji Zillight <banjiolawolez@gmail.com>
2023-03-11 16:55:58 +0100

    A single array in JS can contain all the datatypes

--- seventh.js
+++ seventh.js
@@ -1,10 +1,5 @@
 // Array
 
-const numbers = [ '1', '2', '3'];
+const arr = ["lion", 'eagle', 22, 7.5, true, `wisdom`];
 
-console.log(numbers); // [ '1', '2', '3' ]
-
-const number = [ 1, 2, 3];
-
-console.log(number); // [ 1, 2, 3 ]
-
+console.log(arr); // [ 'lion', 'eagle', 22, 7.5, true, 'wisdom' ]

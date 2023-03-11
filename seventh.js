ceea4464d16d3f91121e350b2efebed3ab115fe4
Banji Zillight <banjiolawolez@gmail.com>
2023-03-11 17:20:13 +0100

    syntax malfunction

--- seventh.js
+++ seventh.js
@@ -5,3 +5,8 @@ let routine = [`eat`, `sleep`];
 
 routine.push(`exercise`);
 console.log(routine); // Prints [ 'eat', 'sleep', 'exercise' ]
+
+// At first I wrote
+// let result = routine.push(`exercise`);
+// console.log(result);
+// And it printed 3, for some strange reasons. I tried it everywhere.

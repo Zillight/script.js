798542824d881eaa53d2d63edc9513701f7a19ca
Banji Zillight <banjiolawolez@gmail.com>
2023-03-11 17:29:12 +0100

    Changing + Pushing  Elements into Array

--- seventh.js
+++ seventh.js
@@ -5,4 +5,5 @@ const routine = [`eat`, `sleep`];
 
 routine[1] = `exercise`;
 console.log(routine); // Prints [ 'eat', 'exercise' ]
-
+routine.push(`pray`);
+console.log(routine); // Prints [ 'eat', 'exercise', 'pray' ]

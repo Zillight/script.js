baded531405eea847a7dcbcec1b2f5d48055642f
Banji Zillight <banjiolawolez@gmail.com>
2023-03-12 23:59:55 +0100

    Array Methods - push() method

---
+++ eighth.js
@@ -0,0 +1,7 @@
+// Array Methods
+// 1. push and unshift() method
+
+const fruits = [`apple`, `mango`, `banana`];
+
+fruits.push(`oranges`);
+console.log(fruits); // Prints [ 'apple', 'mango', 'banana', 'oranges' ]

eb76130f5d7fa8bcc15bf7a2d322bc6be73e4bd2
Banji Zillight <banjiolawolez@gmail.com>
2023-03-13 00:06:11 +0100

    unshift() method

--- eighth.js
+++ eighth.js
@@ -4,4 +4,8 @@
 const fruits = [`apple`, `mango`, `banana`];
 
 fruits.push(`oranges`);
-console.log(fruits); // Prints [ 'apple', 'mango', 'banana', 'oranges' ]
+console.log(fruits); // Logs [ 'apple', 'mango', 'banana', 'oranges' ]
+fruits.unshift(`pawpaw`);
+console.log(fruits); // Logs [ 'pawpaw', 'apple', 'mango', 'banana', 'oranges' ]
+
+// push() takes the argument to the back of the array while unshift() takes it to the front

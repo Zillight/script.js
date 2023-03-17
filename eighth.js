49e3360ca942cf809d60eb47bb5c4701d208c23a
Banji Zillight <banjiolawolez@gmail.com>
2023-03-17 15:01:06 +0100

    slice() parameters/positions

--- eighth.js
+++ eighth.js
@@ -1,6 +1,9 @@
 // Array Methods
 // 5. slice() method
 
-const arr = ["Hello", "Bonjour", 'Salut', 'Oui'];
-const newArr = arr.slice(0,3); // Slice() takes two parameters(start, end+1)
+const arr = ["Hello", "Bonjour", 'Salut', 'Oui', 'Non'];
+let newArr = arr.slice(0,3); // Slice() takes two parameters(start, end+1)
 console.log(newArr); // Prints [ 'Hello', 'Bonjour', 'Salut' ]
+
+newArr = arr.slice(2); // started at position 2 and prints till the end.
+console.log(newArr); // Prints [ 'Salut', 'Oui', 'Non' ] 

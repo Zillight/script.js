e719c209de1a39538ca992812a241e0c2c424694
Banji Zillight <banjiolawolez@gmail.com>
2023-03-17 14:52:30 +0100

    concat() method

--- eighth.js
+++ eighth.js
@@ -1,10 +1,7 @@
 // Array Methods
-// 2. shift() method
+// 3. concat() method
 
-const fruits = [`apple`, `mango`, `banana`];
-
-fruits.pop();
-console.log(fruits); // Logs [ 'apple', 'mango' ]
-
-fruits.shift();
-console.log(fruits); // Logs [ 'mango' ]
+const array1 = ["I", "am"];
+const array2 = ['Banji', `Zillight`];
+const result = array1.concat(array2);
+console.log(result); // Prints [ 'I', 'am', 'Banji', 'Zillight' ]

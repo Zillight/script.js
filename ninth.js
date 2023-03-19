d953756737efbbd52f521a951b7b44b65ac11f5d
Banji Zillight <banjiolawolez@gmail.com>
2023-03-20 00:04:13 +0100

    Map() method Intro

--- ninth.js
+++ ninth.js
@@ -2,14 +2,19 @@
 
 // Example: Multiple the numbers in an array...
 // 1. using for loop
-// Using map () method
 
 const numbers = [1, 2, 3, 4, 5];
+// let squared = [];
+// for (let i = 0; i < numbers.length; i++){
+//     squared.push(numbers[i]*numbers[i]);
+// }
+// console.log(numbers); // Prints [ 1, 4, 9, 16, 25 ]
+// console.log(squared);
 
-let squared = [];
 
-for (let i = 0; i < numbers.length; i++){
-    squared.push(numbers[i]*numbers[i]);
-}
-console.log(numbers);
-console.log(squared);
+//using map() method
+
+const result = numbers.map(function(number){
+    return number * number
+})
+console.log(result); // Prints [ 1, 4, 9, 16, 25 ]

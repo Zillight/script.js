fb3b3c1fd3f23583957a63ae309f5233a9598867
Banji Zillight <banjiolawolez@gmail.com>
2023-03-19 23:58:54 +0100

    using for loop

--- ninth.js
+++ ninth.js
@@ -4,4 +4,12 @@
 // 1. using for loop
 // Using map () method
 
-const 
+const numbers = [1, 2, 3, 4, 5];
+
+let squared = [];
+
+for (let i = 0; i < numbers.length; i++){
+    squared.push(numbers[i]*numbers[i]);
+}
+console.log(numbers);
+console.log(squared);

288b24e4416425cb9db117fac881ec151c16ad43
Banji Zillight <banjiolawolez@gmail.com>
2023-04-03 00:49:37 +0100

    Map method, long function way

--- tenth.js
+++ tenth.js
@@ -2,10 +2,16 @@
 
 // Adding same surname to a all the string elements in an Array
 
-const fullName = [`Banji`, `Doyin`, `Ara`];
-let plusSurname;
-for (let i = 0; i < fullName.length ; i++){
-        plusSurname = fullName[i] + " Olawole";
-    console.log(plusSurname);
-}
-
+// const fullName = [`Banji`, `Doyin`, `Ara`];
+// let plusSurname;
+// for (let i = 0; i < fullName.length ; i++){
+    //         plusSurname = fullName[i] + " Olawole";
+    //     console.log(plusSurname);
+    // }
+    
+    // Using map()
+const names = [`Banji`, `Doyin`, `Ara`];
+const fullNames = names.map(function(name){
+    return name + ` Olawole`;
+})
+console.log(fullNames)
